70b834452526b90306deaab33f90bb9eaa9d43ff
Caden <koscinskic@allegheny.edu>
2020-11-25 16:21:55 -0500

    Refactored mysql and fb credential entry and updated TEMPLATE

--- sns.py
+++ sns.py
@@ -4,25 +4,41 @@ from mysql.connector import errorcode
 from firebase import firebase
 import json
 
-# Fill In the blanks with the relevant info for your test DB or the live DB.
-# In the future I will set this to be loaded from a text file.
-try:
-    dbCon = connector.connect(user = '', password = '', host = '', database = '')
-except connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Wrong user or password!")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Bad Database!")
-    else:
-        print(err)
 
-'''Return an FB connection using provided json config'''
-def firebase_connection(credentials_file_path):
+'''Return a json loaded with necessary FB and mysql credentials'''
+def retrieve_config(credentials_file_path):
 
     with open(credentials_file_path) as f:
-        firebase_config  = json.load(f)
+        config  = json.load(f)
 
-    return firebase.FirebaseApplication(firebase_config.get("databaseURL"), None)
+    return config
+
+
+'''Return a mysqlDB connection '''
+def get_mysqlDB_connection(config):
+
+    dbCon = None
+
+    try:
+        dbCon = connector.connect(
+        user = config.get("mysqlUser"),
+        password = config.get("mysqlPass"),
+        host = config.get("mysqlUser"),
+        database = config.get("mysqlUser"))
+    except connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Wrong user or password!")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Bad Database!")
+        else:
+            print(err)
+
+    return dbCon
+
+
+'''Return an FB connection using provided json config'''
+def get_fb_connection(config_file):
+    return firebase.FirebaseApplication(config_file.get("databaseURL"), None)
 
 
 '''Establish the pipe between GB server and automatically forward to FB'''
@@ -31,7 +47,10 @@ def main():
     # Remove .TEMPLATE from crednetials.json.TEMPLATE and populate as necessary
     CREDENTIALS_FILE_PATH = "credentials.json"
 
-    fb = firebase_connection(CREDENTIALS_FILE_PATH)
+    config = retrieve_config(CREDENTIALS_FILE_PATH)
+
+    fb = get_fb_connection(config)
+    db = get_mysqlDB_connection(config)
 
     print("\nTesting Firebase Connection: \n", fb.get("Machines", None), "\n")
 
