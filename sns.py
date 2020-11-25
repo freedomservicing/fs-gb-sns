e8e7ece978712f2c11068da19087a2273f3f7b1d
Caden <koscinskic@allegheny.edu>
2020-11-25 16:03:34 -0500

    Restructured sns.py and populated TEMPLATE

--- sns.py
+++ sns.py
@@ -2,6 +2,8 @@
 import mysql.connector as connector
 from mysql.connector import errorcode
 from firebase import firebase
+import json
+
 # Fill In the blanks with the relevant info for your test DB or the live DB.
 # In the future I will set this to be loaded from a text file.
 try:
@@ -14,19 +16,25 @@ except connector.Error as err:
     else:
         print(err)
 
-firebase_config = {
-    "apiKey": "",
-    "authDomain": "",
-    "databaseURL": "",
-    "projectId": "",
-    "storageBucket": "",
-    "messagingSenderId": "",
-    "appId": "",
-    "measurementId": ""
-}
+'''Return an FB connection using provided json config'''
+def firebase_connection(credentials_file_path):
 
-fb = firebase.FirebaseApplication(firebase_config.get("databaseURL"), None)
+    with open(credentials_file_path) as f:
+        firebase_config  = json.load(f)
 
-result = fb.get("Machines", None)
+    return firebase.FirebaseApplication(firebase_config.get("databaseURL"), None)
 
-print(result)
+
+'''Establish the pipe between GB server and automatically forward to FB'''
+def main():
+
+    # Remove .TEMPLATE from crednetials.json.TEMPLATE and populate as necessary
+    CREDENTIALS_FILE_PATH = "credentials.json"
+
+    fb = firebase_connection(CREDENTIALS_FILE_PATH)
+
+    print("\nTesting Firebase Connection: \n", fb.get("Machines", None), "\n")
+
+
+if __name__ == "__main__":
+    main()
