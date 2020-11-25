98c53dc65f86c5ec2dc4798af9e5c2c417ce3dd0
Noah <personal.boredhero@gmail.com>
2020-11-25 15:28:32 -0500

    Merge Firebase code

--- sns.py
+++ sns.py
@@ -1,6 +1,7 @@
 # This is *very* rough rn
 import mysql.connector as connector
 from mysql.connector import errorcode
+from firebase import firebase
 # Fill In the blanks with the relevant info for your test DB or the live DB.
 # In the future I will set this to be loaded from a text file.
 try:
@@ -12,3 +13,20 @@ except connector.Error as err:
         print("Bad Database!")
     else:
         print(err)
+
+firebase_config = {
+    "apiKey": "",
+    "authDomain": "",
+    "databaseURL": "",
+    "projectId": "",
+    "storageBucket": "",
+    "messagingSenderId": "",
+    "appId": "",
+    "measurementId": ""
+}
+
+fb = firebase.FirebaseApplication(firebase_config.get("databaseURL"), None)
+
+result = fb.get("Machines", None)
+
+print(result)
