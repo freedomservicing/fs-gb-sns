e0be4d15d0f81727c460313e3780b9692a8f8a65
Noah <personal.boredhero@gmail.com>
2020-12-16 16:26:01 -0500

    Update listener stuff a bit

--- listener.py
+++ listener.py
@@ -4,27 +4,29 @@ from file_manager import file_manager
 
 class Listener:
 
-    
 
-    def __init__(self, table, activeColumn, pollInterval, pageSize, columnList, connector):
-        self.table = table
-        self.activeColumn = activeColumn
-        self.pollInterval = pollInterval
-        self.pageSize = pageSize
-        self.columnList = columnList
-        self.connector = connector
+
+    def __init__(self, table, activeColumn, pollInterval, columnList, connector, pageSize=100):
+        self.__table = table
+        self.__activeColumn = activeColumn
+        self.__pollInterval = pollInterval
+        self.__pageSize = pageSize
+        self.__columnList = columnList
+        self.__connector = connector
+        self.__CACHE_FILE_PATH = "listenerCache.json"
+        self.__CONF_FILE_PATH = "settings.json"
+        self.__cache_manager = file_manager(CACHE_FILE_PATH)
+        self.__conf_manager = file_manager(CONF_FILE_MANAGER)
+        self.__cursor = self.__connector.cursor()
 
     def listen(self):
-        CACHE_FILE_PATH = "listenerCache.json"
-        cache_manager = file_manager(CACHE_FILE_PATH)
-        cursor = self.connector.cursor()
-        query = ("SELECT {self.activeColumn} FROM {self.table}")
-        columnList = ', '.join(self.columnList)
+        # Need a string of columns that contains commas and spacing
+        # Ex:
+        columnList = ', '.join(self.__columnList)
         # TODO: Update this from the file....don't hardcode lastId
         lastId = 0
         while True:
             resultSetSize = 0
-
             while True:
                 # TODO: Figure out proper caching for the interpolation after the > to actually work.
                 query = ("SELECT {columnList} FROM {self.table} WHERE {self.activeColumn} > {See Comment Above} LIMIT {self.PageSize}")
@@ -33,13 +35,7 @@ class Listener:
                 lastId = resultSet.keys(-1)
                 if resultSetSize > 0:
                     break
-            time.sleep(self.pollInterval)
-        
+            time.sleep(self.__pollInterval)
+
+
 ## This needs more work but it's 4:11 and I'm tired. Will resume tomorrow.
-                
-
-        
-        
-
-
-
