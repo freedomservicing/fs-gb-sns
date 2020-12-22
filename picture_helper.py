4a2f33d45dabf25385ed793f7cf42b2d2d6d2145
Noah <personal.boredhero@gmail.com>
2020-12-22 16:57:24 -0500

    Add file refs for firebase

--- picture_helper.py
+++ picture_helper.py
@@ -5,25 +5,34 @@
 ## Helper class for dealing with locally stored images and firebase store
 ## Primary Contributor: Noah
 
+import firebase_admin.storage as storage
+
 class PictureHelper:
 
     # Ex: new PictureHelper("/id_pictures/", "XXXXX-XXX-X-XXXXXXX-X-X", "/batm/data/documents_unsorted/", "DICPHCPMSMXOVEDK7_FRMTVBVD8ZSDRKKO.jpg")
-    def __init__(self, firebase_filename, gb_filename, firebase_filepath = None, gb_filepath = None, extension = None):
+    def __init__(self, firebase_filename, gb_filename, firebase_folder_ref = None, gb_filepath = None, extension = None):
         
         self.__firebase_filename = firebase_filename
         self.__gb_filename = gb_filename
         self.__file_type = gb_filename[20]
         self.__handle_pictures();
+        # https://firebase.google.com/docs/reference/admin/python/firebase_admin.storage
+        # https://cloud.google.com/storage/docs/getting-bucket-information
+        # https://console.cloud.google.com/storage/browser?authuser=1&project=fgcomplianceportal&prefix=
+        # https://firebase.google.com/docs/reference/node/firebase.app
+        self.__storage = storage.bucket("fgcomplianceportal.appspot.com")
+        # https://firebase.google.com/docs/storage/web/create-reference#create_a_reference
+        self.__storage_ref = self.__storage.ref()
+        self.__firebase_folder_ref = None
+
         
         # Handle optional overrides
-        if firebase_filepath is None:
+        if firebase_folder_ref is None:
             if self.__file_type == '2':
-                self.__firebase_filepath = "/id_pictures/"
+                self.__firebase_folder_ref = "id_pictures"
             elif self.__file_type == '6':
-                self.__firebase_filepath = "/customer_selfies/"
+                self.__firebase_folder_ref = "customer_selfies"
             # TODO: Add an error here if it is None but self.__file_type isn't 2 or 6
-        else:
-            self.__firebase_filepath = firebase_filepath
         if gb_filepath is None:
             if self.__file_type == '2':
                 self.__gb_filepath = "/batm/data/documents_unsorted/{self.__gb_filename}"
