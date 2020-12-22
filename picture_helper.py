a98f33d89ff7cf49de2a83f58dbbd629691f0e92
Noah <personal.boredhero@gmail.com>
2020-12-21 23:38:08 -0500

    Handle optional overrides

--- picture_helper.py
+++ picture_helper.py
@@ -10,20 +10,33 @@
 class PictureHelper:
 
 
-    # Ex: new PictureHelper("/id_pictures/", "XXXXX-XXX-X-XXXXXXX-X-X.jpg", "/batm/data/documents_unsorted/", "DICPHCPMSMXOVEDK7_FRMTVBVD8ZSDRKKO.jpg")
+    # Ex: new PictureHelper("/id_pictures/", "XXXXX-XXX-X-XXXXXXX-X-X", "/batm/data/documents_unsorted/", "DICPHCPMSMXOVEDK7_FRMTVBVD8ZSDRKKO.jpg")
     def __init__(self, firebase_filename, gb_filename, firebase_filepath = None, gb_filepath = None, extension = None):
+        
         self.__firebase_filename = firebase_filename
         self.__gb_filename = gb_filename
-        self.__firebase_filepath = firebase_filepath
-        self.__gb_filepath = gb_filepath
-        self.__extension = extension
+        
+        # Handle optional overrides
         self.__file_type = gb_filename[20]
-        if self.__file_type == '2':
-            self.__firebase_filepath = "/id_pictures/"
-            self.__gb_filepath = "/batm/data/documents_unsorted/{self.__gb_filename}"
-        elif self.__file_type == '6':
-            self.__firebase_filepath = "/customer_selfies/"
-            self.__gb_filepath = "/batm/data/captures/{self.__gb_filename}"
+        if firebase_filepath is None:
+            if self.__file_type == '2':
+                self.__firebase_filepath = "/id_pictures/"
+            elif self.__file_type == '6':
+                self.__firebase_filepath = "/customer_selfies/"
+            # TODO: Add an error here if it is None but self.__file_type isn't 2 or 6
         else:
-            ##TODO: Write code to have a descriptive error here.
+            self.__firebase_filepath = firebase_filepath
+        if gb_filepath is None:
+            if self.__file_type == '2':
+                self.__gb_filepath = "/batm/data/documents_unsorted/{self.__gb_filename}"
+            elif self.__file_type == '6':
+                self.__gb_filepath = "/batm/data/captures/{self.__gb_filename}"
+            # TODO: Add an error here if it is None but self.__file_type isn't 2 or 6
+        else:
+            self.__gb_filepath = gb_filepath
+        if extension is None:
+            self.__extension = ".jpg"
+        else:
+            self.__extension = extension
+
 
